e2956a1a1b3d7b176a257ccf823b348eb83c4cf8
ErikaLim <Erika.megan.lim@gmail.com>
2014-12-17 15:58:51 -0800

    fixed dropdowns

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
   root to: "employments#index"
   resources :employments
+  resources :people
+  resources :locations
+  resources :job_titles
+  resources :organizations
 end
